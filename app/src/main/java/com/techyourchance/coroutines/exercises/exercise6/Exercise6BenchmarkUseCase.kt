47ed06f34c7a144bdbdcbab8ae3934580b4fd2d0
Alvin Dizon <admdizon@scentregroup.com>
2021-09-05 13:38:19 +0800

    Solution for Exercise 6
    Signed-off-by: Alvin Dizon <admdizon@scentregroup.com>

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6BenchmarkUseCase.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6BenchmarkUseCase.kt
@@ -1,20 +1,20 @@
 package com.techyourchance.coroutines.exercises.exercise6
 
-import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.isActive
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 
 class Exercise6BenchmarkUseCase(private val postBenchmarkResultsEndpoint: PostBenchmarkResultsEndpoint) {
 
-    suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.Default) {
+    suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.IO) {
         logThreadInfo("benchmark started")
 
         val stopTimeNano = System.nanoTime() + benchmarkDurationSeconds * 1_000_000_000L
 
         var iterationsCount: Long = 0
         while (System.nanoTime() < stopTimeNano) {
+            ensureActive()
             iterationsCount++
         }
 
